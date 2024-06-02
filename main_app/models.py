144b7c43bc969d50c346a5c7fa507ba0b41239f9
kiibra <kibra99@gmail.com>
2024-06-02 09:19:15 -0400

    add str method to model

--- main_app/models.py
+++ main_app/models.py
@@ -7,3 +7,7 @@ class Diary(models.Model):
   content = models.TextField(max_length=500)
   created_at = models.DateField.auto_now_add()
   updated_at = models.DateField.auto_now()
+
+	# ovverride __str__ method to return the diary title attribute
+  def __str__(self):
+    return self.title
