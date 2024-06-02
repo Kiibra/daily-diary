8608a593c6254af19430acfe313b51e644bd60f9
kiibra <kibra99@gmail.com>
2024-06-02 12:08:58 -0400

    update index view

--- main_app/views.py
+++ main_app/views.py
@@ -2,19 +2,6 @@ from django.shortcuts import render
 from .models import Diary
 
 
-class Diary:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, created_at, title, content, updated_at):
-    self.title = title
-    self.content = content
-    self.created_at = created_at
-    self.updated_at = updated_at
-
-diaries = [
-  Diary('06/01/2024', 'thoughts', 'today i am learning and building my first django app.', '06/01/2024' ),
-  Diary('05/20/2014', 'my day', 'today was such a rainy and sad day. I stayed in all day!', '05/31/2024' ),
-]
-
-
 # Create your views here.
 
 # Define the home view
@@ -25,11 +12,10 @@ def home(request):
 def about(request):
     return render(request, 'about.html')
 
-# Add new view
 def diary_index(request):
-  # diaries = Diary.objects.all()
+  diaries = Diary.objects.all()
   return render(request, 'diaries/index.html', { 'diaries': diaries })
 
-def diary_detail(request, diary_id):
-  diary = Diary.objects.get(id=diary_id)
-  return render(request, 'diaries/detail.html', { 'diary': diary })
+# def diary_detail(request, diary_id):
+#   diary = Diary.objects.get(id=diary_id)
+#   return render(request, 'diaries/detail.html', { 'diary': diary })
