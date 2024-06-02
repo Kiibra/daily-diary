1bc5ecfd11c345b68033e1fa600fa71c70029261
kiibra <kibra99@gmail.com>
2024-06-01 22:34:43 -0400

    define view function for Home Page

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+# Add the HttpResponse
+from django.http import HttpResponse
+
 
 # Create your views here.
+
+# Define the home view
+def home(request):
+  return HttpResponse('<h1>Welcome to your Daily Diaries 🦋</h1>')
