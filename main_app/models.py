115da90e3e4c716dd17c0da8e7f5955dceece550
kiibra <kibra99@gmail.com>
2024-06-02 09:25:40 -0400

    fix model dateField & make + run  migrations

--- main_app/models.py
+++ main_app/models.py
@@ -5,8 +5,8 @@ from django.db import models
 class Diary(models.Model):
   title = models.CharField(max_length=100)
   content = models.TextField(max_length=500)
-  created_at = models.DateField.auto_now_add()
-  updated_at = models.DateField.auto_now()
+  created_at = models.DateField(auto_now_add=True)
+  updated_at = models.DateField(auto_now=True)
 
 	# ovverride __str__ method to return the diary title attribute
   def __str__(self):
