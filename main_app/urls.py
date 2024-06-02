f7a584ecb103a44a04bd8d76b4d4ceac8cfcd3d7
kiibra <kibra99@gmail.com>
2024-06-02 12:52:47 -0400

    add route and create UI to add a diary

--- main_app/urls.py
+++ main_app/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('diaries/', views.diary_index, name='diary-index'),
     # new route for details 
     path('diaries/<int:diary_id>/', views.diary_detail, name='diary-detail'),
+    # new route to show a form and create a diary
+    path('diary/create/', views.DiaryCreate.as_view(), name='diary-create'),
 ]
