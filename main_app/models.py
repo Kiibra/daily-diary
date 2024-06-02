10664e648beea0423781c1c14840f5464026c0e0
kiibra <kibra99@gmail.com>
2024-06-02 15:11:44 -0400

    add Tag model

--- main_app/models.py
+++ main_app/models.py
@@ -4,11 +4,20 @@ from django.urls import reverse
 
 # Create your models here.
 
+class Tag(models.Model):
+    name = models.CharField(
+      max_length=50, 
+      unique=True)
+    
+    def __str__(self):
+        return self.name
+
 class Diary(models.Model):
   title = models.CharField(max_length=100)
   content = models.TextField(max_length=500)
   created_at = models.DateField(auto_now_add=True)
   updated_at = models.DateField(auto_now=True)
+  tags = models.ManyToManyField(Tag, related_name='diaries')
 
 	# ovverride __str__ method to return the diary title attribute
   def __str__(self):
